fd998b6fd7f3551692bfff34649f58e97fcf765f
Eppywambui <epiphaniawambui@gmail.com>
2024-02-26 21:47:37 +0300

    plp learning

---
+++ Python/user_input.py
@@ -0,0 +1,8 @@
+name = (input("What is your name?: "))
+age = (input("What is your age?: "))
+location = (input("What is your location?; "))
+
+print("My name is", name)
+print("My age is", age)
+print("I live in", location)
+      
