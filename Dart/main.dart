f799856379e03743c351f1799189db2f90f76abc
EppyWambui <epiphaniawambui@gmail.com>
2024-02-28 14:50:52 +0300

    my first dart file

---
+++ Dart/main.dart
@@ -0,0 +1,31 @@
+//Data types in dart
+//int and double
+
+void main () {
+  //Declaring variables
+  int num1 = 10; //without decimal
+  double num2 = 10.3; //with decimal
+  num num3 = 50; //both with decimal and without
+
+  //sum
+  num sum = num1 + num2 + num3;
+
+  //printing 
+  print("Number one is $num1");
+  print("Number 2 is $num2");
+  print("Number 3 is $num3");
+  print("Sum is $sum");
+
+  //strings
+  String SchoolName = "Breuburn";
+  String Address = "Nairobi";
+
+  print("My school name is $SchoolName and location is $Address");
+
+//bool
+bool IsMarried = false;
+
+print("are you married? $IsMarried");
+
+//
+}
